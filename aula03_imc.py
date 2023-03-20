5d7c1594fe0ba68c54330d3696ec55cd126fa286
Leandro EN <leandro.nisiyama100@gmail.com>
2023-03-19 23:05:20 -0300

    Correções sintaxe condicionais

--- aula03_imc.py
+++ aula03_imc.py
@@ -7,16 +7,16 @@ print(nome)
 print("Seu IMC é de", imc)
 
 print("Resultado: ")
-if(imc <= 18.5):  
+if imc <= 18.5:  
     print("Magreza")
-if(imc > 18.5 and imc <= 24.9):
+elif imc > 18.5 and imc <= 24.9:
     print("Normal")
-if(imc > 24.9 and imc <= 29.9):
+elif imc > 24.9 and imc <= 29.9:
     print("Sobrepeso")
-if(imc > 29.9 and imc <= 34.9):
+elif imc > 29.9 and imc <= 34.9:
     print("Obesidade Grau I")
-if(imc > 34.9 and imc <= 39.9):
+elif imc > 34.9 and imc <= 39.9:
     print("Obesidade Grau II")
-if(imc > 39.9):
+else:
     print("Obesidade Grau III")
 
--- aula04_imc_fStrings.py
+++ aula04_imc_fStrings.py
@@ -4,18 +4,18 @@ peso = 95
 
 imc = peso / ( altura ** 2 )
 
-if(imc <= 18.5):  
-    linha3 = 'Magro'
-if(imc > 18.5 and imc <= 24.9):
+if imc <= 18.5:  
+    linha3 = 'Magreza'
+elif imc > 18.5 and imc <= 24.9:
     linha3 = 'Normal'
-if(imc > 24.9 and imc <= 29.9):
-    linha3 = 'Com Sobrepeso'
-if(imc > 29.9 and imc <= 34.9):
-    linha3 = 'Com Obesidade Grau I'
-if(imc > 34.9 and imc <= 39.9):
-    linha3 = 'Com Obesidade Grau II'
-if(imc > 39.9):
-    linha3 = 'Com Obesidade Grau III'
+elif imc > 24.9 and imc <= 29.9:
+    linha3 = 'Sobrepeso'
+elif imc > 29.9 and imc <= 34.9:
+    linha3 = 'Obesidade Grau I'
+elif imc > 34.9 and imc <= 39.9:
+    linha3 = 'Obesidade Grau II'
+else:
+    linha3 = 'Obesidade Grau III'
 
 linha1 = f'{nome}, você está atualmente com {peso:.2f} Kg. '
 linha2 = f'Seu IMC é de {imc:.2f} e você está:'
