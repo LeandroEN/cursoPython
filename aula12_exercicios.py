f84b99758fda871a430114cca0c440bce4c5f67a
Leandro EN <leandro.nisiyama100@gmail.com>
2023-03-26 02:51:44 -0300

    exericio feito e correção do exercício

---
+++ aula12_exercicios.py
@@ -0,0 +1,67 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+string_nome = str(nome)
+idade_int = int(idade)
+
+nomeInvertido = nome[::-1]
+contemEspacoNome = ' ' in string_nome
+qtdeLetrasNome = len(string_nome)
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nomeInvertido}')
+
+    if(not contemEspacoNome): 
+        print(f'Seu nome não contém espaços')
+    else:
+        print(f'Seu nome contém espaços')
+
+    print(f'Seu nome tem {qtdeLetrasNome} letras')
+    print(f'A primeira letra do seu nome é {string_nome[0]}')
+    print(f'A última letra do seu nome é {string_nome[-1]}')
+else:
+    print('Desculpe, você deixou campos vazios!')
+
+
+    '''
+    Correção do exercício:
+
+    Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+nome = input('Digite o seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome NÃO contém espaços')
+
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+else:
+    print("Desculpe, você deixou campos vazios.")
+    '''
